2bfd0fc4aac70f16f4fb0b331dcba8c81e67c846
joomlagovbr <rafael.berlanda@mec.gov.br>
2013-10-31 13:42:40 -0200

    Atualização de código conforme o disposto em: http://epwg.governoeletronico.gov.br/barra/

--- tmpl/default.php
+++ tmpl/default.php
@@ -11,12 +11,12 @@ defined('_JEXEC') or die;
 ?>
 <!-- barra do governo -->
 <div id="barra-brasil">
-    <a href="http://brasil.gov.br" title="Acesse para conhecer todos os serviços e informações do Governo Brasileiro na Internet.">Portal do Governo Brasileiro</a> 
-    <?php if( $anexar_js == 1 && $endereco_js != ""): ?>
-	<script src="<?php echo $endereco_js; ?>" type="text/javascript"></script><noscript>A barra do Governo Federal só poderá ser visualizada se o javascript estiver ativado.</noscript>
-	<?php endif; ?>	
+    <a href="http://brasil.gov.br" style="background:#7F7F7F; height: 20px; padding:4px 0 4px 10px; display: block; font-family:sans,sans-serif; text-decoration:none; color:white; ">Portal do Governo Brasileiro</a>
 </div>
+<?php if( $anexar_js == 1 && $endereco_js != ""): ?>
+<script src="<?php echo $endereco_js; ?>" type="text/javascript"></script><noscript>A barra do Governo Federal só poderá ser visualizada se o javascript estiver ativado.</noscript>
+<?php endif; ?>	
 <?php if( $mensagem_ie_6 != ""): ?>
 <!--[if lt IE 7]><br /><strong><?php echo $mensagem_ie_6; ?></strong><br /><![endif]-->
 <?php endif; ?>
-<!-- fim barra do governo -->    
+<!-- fim barra do governo -->
